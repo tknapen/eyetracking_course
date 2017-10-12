a1626d88d22cd5afdbd4e60712c52ac55a504d47
Tomas Knapen <tknapen@gmail.com>
2017-10-12 15:53:43 +0200

    Use direction (still broken)

--- experiments/PR/trial.py
+++ experiments/PR/trial.py
@@ -11,18 +11,22 @@ class PRTrial(Trial):
     def __init__(self, ti, config, parameters, *args, **kwargs):
 
         self.ID = ti
+        # self.parameters = parameters
 
         phase_durations = [parameters['fixation_duration'], 
                            parameters['random_dots1_duration'],
                            parameters['coherent_dots_duration'],
                            parameters['random_dots2_duration']]
-
+                           
         super(
             PRTrial,
             self).__init__(
             phase_durations=phase_durations,
+            parameters = parameters,
             *args,
             **kwargs)
+
+
 
     def draw(self, *args, **kwargs):
 
@@ -63,5 +67,6 @@ class PRTrial(Trial):
             self.session.dots.coherence = 0.0
         elif self.phase == 2:
             self.session.dots.coherence = 1.0
+            self.session.dots.direction = self.parameters['direction']
         elif self.phase == 3:
             self.session.dots.coherence = 0.0
